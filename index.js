94304078ec75ba260db3ab50fc035de77b4bb06b
Megan Harrison <mtharrison@alumni.fullsail.edu>
2023-04-26 17:21:35 -0500

    complete lab

---
+++ index.js
@@ -0,0 +1,36 @@
+// add function
+function add (a, b) {
+    return a + b;
+}
+// subtract function
+function subtract (a, b) {
+    return a - b;
+}
+// multiplication function
+function multiply (a, b){
+    return a * b;
+}
+// division function
+function divide (a, b){
+    return a / b;
+}
+// increment
+function increment(a) {
+    a ++;
+    return a;
+}
+// decrement
+function decrement(a) {
+     a --;
+    return a;
+}
+// parseint
+function makeInt(a) {
+    let number = parseInt(a, 10);
+    return number;
+}
+// parsefloat
+function preserveDecimal(a) {
+   let number = parseFloat(a)
+   return number;
+}
